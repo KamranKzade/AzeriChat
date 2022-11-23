6bd6aca2a5b1518b2b3df90122bfa3f8f587c690
Kamran <311.kamran@gmail.com>
2022-11-23 05:15:52 +0400

    Add project files.

---
+++ AzeriChat/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AzeriChat;
+
+
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        contact_stackpanel.Children.Add();
+       
+    }
+}
