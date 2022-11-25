1d9501e8fb9433065c4b2fe15eabff9bc3cd6893
Kamran <311.kamran@gmail.com>
2022-11-26 02:05:00 +0400

    Complete

--- AzeriChat/Repository/FakeRepo.cs
+++ AzeriChat/Repository/FakeRepo.cs
@@ -11,8 +11,7 @@ public class FakeRepo
             {
                 new Human
                 {
-                    //ImageUrl=@"/Images/ronaldo.png",
-                    ImageUrl=@"C:\Users\Kamran\source\repos\AzeriChat\AzeriChat\Images\ronaldo.png",
+                    ImageUrl=@"../../../Images/ronaldo.png",
                     Name = "Ronaldo",
                     Message = "Come to Real Madrid",
                     MessageCount = "1",
@@ -20,8 +19,7 @@ public class FakeRepo
                 },
                 new Human
                 {
-                    //ImageUrl=@"/Images/messi.png",
-                    ImageUrl=@"C:\Users\Kamran\source\repos\AzeriChat\AzeriChat\Images\messi.png",
+                    ImageUrl=@"../../../Images/messi.png",
                     Name = "Messi",
                     Message = "Come to PSG",
                     MessageCount = null,
@@ -29,8 +27,7 @@ public class FakeRepo
                 },
                 new Human
                 {
-                    //ImageUrl=@"/Images/alex.png",
-                    ImageUrl=@"C:\Users\Kamran\source\repos\AzeriChat\AzeriChat\Images\alex.png",
+                    ImageUrl=@"../../../Images/alex.png",
                     Name = "Alex",
                     Message = "Come to Fenerbagce",
                     MessageCount = null,
@@ -38,8 +35,7 @@ public class FakeRepo
                 },
                 new Human
                 {
-                    //ImageUrl=@"/Images/benzema.png",
-                    ImageUrl=@"C:\Users\Kamran\source\repos\AzeriChat\AzeriChat\Images\benzema.png",
+                    ImageUrl=@"../../../Images/benzema.png",
                     Name = "Benzema",
                     Message = "Come to Real Madrid",
                     MessageCount = null,
@@ -47,8 +43,7 @@ public class FakeRepo
                 },
                 new Human
                 {
-                    //ImageUrl=@"/Images/maradona.png",
-                    ImageUrl=@"C:\Users\Kamran\source\repos\AzeriChat\AzeriChat\Images\maradona.png",
+                    ImageUrl=@"../../../Images/maradona.png",
                     Name = "Maradona",
                     Message = "Come to Napoli",
                     MessageCount = "23",
@@ -56,8 +51,7 @@ public class FakeRepo
                 },
                 new Human
                 {
-                    //ImageUrl=@"/Images/neymar.png",
-                    ImageUrl=@"C:\Users\Kamran\source\repos\AzeriChat\AzeriChat\Images\neymar.png",
+                    ImageUrl=@"../../../Images/neymar.png",
                     Name = "Neymar",
                     Message = "Come to PSG",
                     MessageCount = "8",
@@ -65,8 +59,7 @@ public class FakeRepo
                 },
                 new Human
                 {
-                    //ImageUrl=@"/Images/seydayev.png",
-                    ImageUrl=@"C:\Users\Kamran\source\repos\AzeriChat\AzeriChat\Images\seydayev.png",
+                    ImageUrl=@"../../../Images/seydayev.png",
                     Name = "Ramil Sheydayev",
                     Message = "Come to Karabag",
                     MessageCount = null,
